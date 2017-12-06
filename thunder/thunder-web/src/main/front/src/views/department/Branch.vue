d7b6ce6380d3d3b75b490dea5c94506690f5a276
kerbores <kerbores@zhcs.club>
2017-12-06 17:46:46 +0800

    add:百度地图组件

--- thunder/thunder-web/src/main/front/src/views/department/Branch.vue
+++ thunder/thunder-web/src/main/front/src/views/department/Branch.vue
@@ -1,11 +1,27 @@
 <template>
     <section>
         机构管理
+        <b-map-component :ak="ak"  @notify="notify"></b-map-component>
     </section>
 </template>
 <style>
 
 </style>
 <script>
-export default {};
+import BMapComponent from "@/components/Map/BaiduMap.vue";
+export default {
+  data() {
+    return {
+      ak: "CRHkMGE7Db1USNSyFXqVDmdv"
+    };
+  },
+  methods: {
+    notify(rs) {
+      console.log(rs);
+    }
+  },
+  components: {
+    BMapComponent
+  }
+};
 </script>
