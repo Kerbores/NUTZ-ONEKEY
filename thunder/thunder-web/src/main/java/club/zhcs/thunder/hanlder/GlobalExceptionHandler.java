55774a26e858b89ba2e22c8dd99db3a3c4d337a2
kerbores <kerbores@zhcs.club>
2017-12-08 11:17:41 +0800

    fix:前端页面刷新404

--- thunder/thunder-web/src/main/java/club/zhcs/thunder/hanlder/GlobalExceptionHandler.java
+++ thunder/thunder-web/src/main/java/club/zhcs/thunder/hanlder/GlobalExceptionHandler.java
@@ -4,6 +4,7 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.apache.log4j.Logger;
 import org.apache.shiro.authz.UnauthenticatedException;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,7 +20,7 @@ public class GlobalExceptionHandler {
 	public Result defaultErrorHandler(HttpServletResponse response, Exception e) throws Exception {
 		logger.error("error=>", e);
 		if (e instanceof UnauthenticatedException) {
-			response.setStatus(401);
+			response.setStatus(HttpStatus.UNAUTHORIZED.value());
 			return Result.fail("没有权限进行此操作!");
 		}
 		return Result.exception(e);
