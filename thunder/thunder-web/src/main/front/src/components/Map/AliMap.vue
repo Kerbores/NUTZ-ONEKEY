d771201bcd5473ffb498c55b01ab51fdbd66e72d
kerbores <kerbores@zhcs.club>
2017-12-12 16:16:39 +0800

    add:搞定机构

--- thunder/thunder-web/src/main/front/src/components/Map/AliMap.vue
+++ thunder/thunder-web/src/main/front/src/components/Map/AliMap.vue
@@ -1,8 +1,12 @@
 <template>
     <div class="amap-page-container">
-        <el-amap-search-box class="search-box" :search-option="searchOption"
+        <el-amap-search-box class="search-box" 
+                            :search-option="searchOption"
                             :on-search-result="onSearchResult"></el-amap-search-box>
-        <el-amap :plugin="plugin" vid="ali-map" :center="mapCenter" :events="events" :zoom="12" class="ali-map"
+        <el-amap :plugin="plugin" vid="ali-map" 
+                 :center="mapCenter" 
+                 :events="events" 
+                 :zoom="12" class="ali-map"
                  :class="mapClass">
             <el-amap-marker :position="mapCenter"></el-amap-marker>
         </el-amap>
@@ -40,6 +44,11 @@
 <script>
 export default {
   props: {
+    center: {
+      type: Array,
+      default:function(){return [0,0]},
+      required:false
+    },
     mapClass: {
       type: String,
       default: "",
@@ -53,7 +62,7 @@ export default {
         city: "重庆",
         citylimit: false
       },
-      mapCenter: [121.59996, 31.197646],
+      mapCenter: [this.center[0], this.center[1]],
       events: {
         click(e) {
           let { lng, lat } = e.lnglat;
@@ -82,13 +91,17 @@ export default {
           pName: "Geolocation",
           events: {
             init(o) {
-              // o 是高德地图定位插件实例
-              o.getCurrentPosition((status, result) => {
-                if (result && result.position) {
-                  self.geocoder(result.position.lng, result.position.lat);
-                  self.$nextTick();
-                }
-              });
+              if (self.mapCenter[0] === 0 && self.mapCenter[1] === 0) {
+                o.getCurrentPosition((status, result) => {
+                  if (result && result.position) {
+                    self.geocoder(result.position.lng, result.position.lat);
+                    self.$nextTick();
+                  }
+                });
+              } else {
+                self.geocoder(self.mapCenter[0], self.mapCenter[1]);
+                self.$nextTick();
+              }
             }
           }
         }
@@ -96,10 +109,13 @@ export default {
     };
   },
   methods: {
+    reset(){
+      this.center = [0,0];
+    },
     notify(rs) {
       this.$emit("locationSelected", rs);
     },
-    geocoder(lng, lat) {
+    lacationAndnotify(lng, lat){
       let self = this;
       var geocoder = new AMap.Geocoder({
         radius: 1000,
@@ -118,6 +134,18 @@ export default {
           }
         }
       });
+    },
+    geocoder(lng, lat) {
+      let self = this;
+      if(lng === 0 && lat ===0){
+        new AMap.Geolocation().getCurrentPosition((status, result) => {
+                  if (result && result.position) {
+                    self.lacationAndnotify(result.position.lng, result.position.lat);
+                  }
+                });
+      }else{
+        self.lacationAndnotify(lng, lat);
+      }
     },
     onSearchResult(pois) {
       let latSum = 0;
--- thunder/thunder-web/src/main/front/src/views/department/Branch.vue
+++ thunder/thunder-web/src/main/front/src/views/department/Branch.vue
@@ -7,9 +7,7 @@
           </el-button>
         </el-input>
       </el-col>
-      <el-col 
-        :span="6" 
-        :offset="10">
+      <el-col :span="6" :offset="10">
         <el-button type="primary" icon="el-icon-fa-plus" size="small" @click="addBranch">
           添加机构
         </el-button>
@@ -23,13 +21,13 @@
       </el-table-column>
       <el-table-column label="操作" show-overflow-tooltip header-align="center" align="center">
         <template slot-scope="scope">
-                      <el-button-group>
-                          <el-button title="编辑码本" size="mini" type="primary" icon="el-icon-fa-edit"
-                                     @click="handleEdit(scope.$index,scope.row)"></el-button>
-                          <el-button title="删除码本" size="mini" type="primary"
-                                     icon="el-icon-fa-trash" @click="handleDelete(scope.$index,scope.row)"></el-button>
-                      </el-button-group>
-          </template>
+                        <el-button-group>
+                            <el-button title="编辑码本" size="mini" type="primary" icon="el-icon-fa-edit"
+                                       @click="handleEdit(scope.$index,scope.row)"></el-button>
+                            <el-button title="删除码本" size="mini" type="primary"
+                                       icon="el-icon-fa-trash" @click="handleDelete(scope.$index,scope.row)"></el-button>
+                        </el-button-group>
+</template>
             </el-table-column>
         </el-table>
 
@@ -52,6 +50,7 @@
         <el-dialog 
           :title="branch.id == 0 ? '添加机构' : '编辑机构' " 
           :visible.sync="addEditShow" 
+          @close="resetBranch"
           width="45%">
             <el-form 
               :model="branch" 
@@ -67,6 +66,7 @@
                       ref="tree"
                       highlight-current 
                       :props="defaultProps" 
+                      :default-checked-keys="[branch.parentId]"
                       @check-change="check">
                     </el-tree>
                 </el-form-item>
@@ -82,11 +82,13 @@
                               v-model="branch.description" auto-complete="off"></el-input>
                 </el-form-item>
                 <el-form-item label="地址信息" :label-width="formLabelWidth" prop="address">
-                    <ali-map @locationSelected="notify"></ali-map>
+                    <ali-map :center="location" 
+                              @locationSelected="notify"
+                              ref="aliMap"></ali-map>
                 </el-form-item>
             </el-form>
             <div slot="footer" class="dialog-footer">
-                <el-button @click="addEditShow = false ;">取 消</el-button>
+                <el-button @click="addEditShow = false ; resetBranch()">取 消</el-button>
                 <el-button type="primary" @click="saveOrUpdateBranch('branchForm')">确 定</el-button>
             </div>
         </el-dialog>
@@ -98,154 +100,164 @@
 </style>
 
 <script>
-import AliMap from "@/components/Map/AliMap";
-export default {
-  data() {
-    return {
-      topBranch: [],
-      defaultProps: {
-        children: "children",
-        label: "name"
-      },
-      searchKey: "",
-      addEditShow: false,
-      pager: {
+  import AliMap from "@/components/Map/AliMap";
+  export default {
+    data() {
+      return {
+        topBranch: [],
+        defaultProps: {
+          children: "children",
+          label: "name"
+        },
+        searchKey: "",
+        addEditShow: false,
         pager: {
-          pageCount: 0,
-          pageNumber: 1,
-          pageSize: 15,
-          recordCount: 0
+          pager: {
+            pageCount: 0,
+            pageNumber: 1,
+            pageSize: 15,
+            recordCount: 0
+          }
+        },
+        branch: {
+          code: this.$utils.guid()
+        },
+        formLabelWidth: "120px"
+      };
+    },
+    computed: {
+      location() {
+        return [this.branch.longitude || 0, this.branch.latitude || 0];
+      }
+    },
+    methods: {
+      resetBranch() {
+        this.branch = {
+          code: this.$utils.guid()
         }
       },
-      branch: {
-        code: this.$utils.guid()
+      addBranch() {
+        this.addEditShow = true;
+        this.$refs.aliMap && this.$refs.aliMap.geocoder(0, 0);
       },
-      formLabelWidth: "120px"
-    };
-  },
-  methods: {
-    addBranch() {
-      this.addEditShow = true;
-    },
-    check(data, checked, indeterminate) {
-      if (this.$refs.tree.getCheckedNodes().length > 1) {
-        this.$message("只能选择一个上级机构");
-        this.$refs.tree.setChecked(data, false);
-      } else if (checked) {
-        this.branch.parentId = data.id;
-      } else {
-        this.branch.parentId = 0;
-      }
-    },
-    loadBranch(node, resolve) {
-      if (node.level === 0) {
-        this.$api.Branch.top(result => {
-          resolve(result.tops);
-        });
-      } else {
-        this.$api.Branch.sub(node.data.id, result => {
-          resolve(result.branchs);
-        });
-      }
-    },
-    saveOrUpdateBranch(formName) {
-      this.$refs[formName].validate(valid => {
-        if (valid) {
-          var callback = result => {
-            this.changePage();
-            this.addEditShow = false;
-          };
-          this.branch.id
-            ? this.$api.Branch.update(this.branch, callback)
-            : this.$api.Branch.save(this.branch, callback);
+      check(data, checked, indeterminate) {
+        if (this.$refs.tree.getCheckedNodes().length > 1 && checked) {
+          this.$message("只能选择一个上级机构");
+          this.$refs.tree.setChecked(data, false);
+        } else if (checked) {
+          this.branch.parentId = data.id;
         } else {
-          return false;
+          this.branch.parentId = 0;
         }
-      });
-    },
-    remote(row, callback) {
-      this.$api.Branch.sub(row.id, result => {
-        const data = [];
-        result.branchs.forEach(item => {
-          item.children = [{}];
-          item.depth = row.depth ? row.depth + 1 : 1;
-          data.push(item);
+      },
+      loadBranch(node, resolve) {
+        if (node.level === 0) {
+          this.$api.Branch.top(result => {
+            resolve(result.tops);
+          });
+        } else {
+          this.$api.Branch.sub(node.data.id, result => {
+            resolve(result.branchs);
+          });
+        }
+      },
+      handleEdit(index, row) {
+        this.addEditShow = true;
+        this.$refs.aliMap && this.$refs.aliMap.geocoder(row.longitude, row.latitude);
+        this.branch = row;
+      },
+      handleDelete(index, row) {
+        this.$confirm("确认删除机构?", "删除确认", {
+          confirmButtonText: "确定",
+          cancelButtonText: "取消",
+          type: "warning"
+        }).then(() => {
+          this.$api.Branch.delete(row.id, result => {
+            this.$message({
+              type: "success",
+              message: "删除成功!"
+            });
+            window.setTimeout(() => {
+              this.changePage();
+            }, 2000);
+          });
         });
-        callback(data);
-      });
-    },
-    changePage() {
-      if (this.searchKey) {
-        this.doSearch();
-      } else {
-        this.loadData();
-      }
-    },
-    doSearch() {
-      this.$api.Branch.search(
-        this.pager.pager.pageNumber,
-        this.searchKey,
-        result => {
+      },
+      saveOrUpdateBranch(formName) {
+        this.$refs[formName].validate(valid => {
+          if (valid) {
+            var callback = result => {
+              this.changePage();
+              this.addEditShow = false;
+            };
+            this.$confirm("你选择的地址是 "+this.branch.address+" ?", "地址信息确认", {
+              confirmButtonText: "确定",
+              cancelButtonText: "取消",
+              type: "warning"
+            }).then(() => {
+              this.branch.id ?
+                this.$api.Branch.update(this.branch, callback) :
+                this.$api.Branch.save(this.branch, callback);
+            });
+          } else {
+            return false;
+          }
+        });
+      },
+      remote(row, callback) {
+        this.$api.Branch.sub(row.id, result => {
+          const data = [];
+          result.branchs.forEach(item => {
+            item.children = [{}];
+            item.depth = row.depth ? row.depth + 1 : 1;
+            data.push(item);
+          });
+          callback(data);
+        });
+      },
+      changePage() {
+        if (this.searchKey) {
+          this.doSearch();
+        } else {
+          this.loadData();
+        }
+      },
+      doSearch() {
+        this.$api.Branch.search(
+          this.pager.pager.pageNumber,
+          this.searchKey,
+          result => {
+            this.pager = result.pager;
+            this.pager.dataList.forEach(item => {
+              item.children = [{}];
+              item.depth = 1;
+            });
+          }
+        );
+      },
+      loadData() {
+        this.$api.Branch.list(this.pager.pager.pageNumber, result => {
           this.pager = result.pager;
           this.pager.dataList.forEach(item => {
             item.children = [{}];
             item.depth = 1;
           });
-        }
-      );
-    },
-    loadData() {
-      this.$api.Branch.list(this.pager.pager.pageNumber, result => {
-        this.pager = result.pager;
-        this.pager.dataList.forEach(item => {
-          item.children = [{}];
-          item.depth = 1;
         });
-      });
-    },
-    notify(rs) {
-      this.branch.longitude = rs.point.lng;
-      this.branch.latitude = rs.point.lat;
-      this.branch.province = rs.address.province;
-      this.branch.city = rs.address.city;
-      this.branch.district = rs.address.district;
-      this.branch.address = rs.formattedAddress;
-    }
-  },
-  created() {
-    this.loadData();
-  },
-  components: {
-    AliMap
-  }
-};
-</script>
-
-<!--<template>
-  <section>
-    机构管理
-    <ali-map @locationSelected="notify"></ali-map>
-  </section>
-</template>
-
-<style>
-
-</style>
-
-<script>
-  import AliMap from "@/components/Map/AliMap";
-  export default {
-    data() {
-      return {};
-    },
-    methods: {
+      },
       notify(rs) {
-        console.log(rs);
+        this.branch.longitude = rs.point.lng;
+        this.branch.latitude = rs.point.lat;
+        this.branch.province = rs.address.province;
+        this.branch.city = rs.address.city;
+        this.branch.district = rs.address.district;
+        this.branch.address = rs.formattedAddress;
       }
     },
-    created() {},
+    created() {
+      this.loadData();
+    },
     components: {
       AliMap
     }
   };
-</script>-->
+</script>
